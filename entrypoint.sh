f7c5005c9cba47e01707b903b0bee4ea123ff7e7
Thomas Smallwood <thomasinio@hotmail.com>
2020-12-29 19:55:30 +0000

    remove unnecessary entrypoint comments

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,4 @@
 #!/bin/sh
 
-# Give appuser access to local volume
-# groupmod -g `ls -ln /app | grep music | grep -v grep | awk ' { print $4 }'` appuser
-
-# Switch to non-root user
-# su - appuser
-
-# Start program
+# Start the program
 eval $( python3.6 -m autodj.main )
